d3f38c77320b1fdd0a2b41f481d25ec0be3a4da6
Pavel Cherdantsev <ch3pash@yandex.ru>
2023-04-24 19:00:39 +0400

    feat: add component Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,29 @@
 function Footer() {
   return (
     <footer className="footer">
-      <p>sdfsdf</p>
+      <div className="footer__container page__container">
+        <h2 className="footer__heading">
+          Учебный проект Яндекс.Практикум х BeatFilm.
+        </h2>
+        <div className="footer__wrapper">
+          <p className="footer__copyright">&copy; {new Date().getFullYear()}</p>
+          <ul className="footer__list page__list">
+            <li className="footer__item">
+              <a href="https://practicum.yandex.ru/" className="footer__link">
+                Яндекс.Практикум
+              </a>
+            </li>
+            <li className="footer__item">
+              <a
+                href="https://github.com/chepash/movies-explorer-frontend"
+                className="footer__link"
+              >
+                Github
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
     </footer>
   );
 }
