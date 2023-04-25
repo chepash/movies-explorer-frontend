6c0939aeb0f1af9bccb2dac2a1e50ae117eb0a02
Pavel Cherdantsev <ch3pash@yandex.ru>
2023-04-26 00:31:58 +0400

    feat: add remove card btn on route /saved-movies

--- src/components/Movies/MoviesCard/MoviesCard.js
+++ src/components/Movies/MoviesCard/MoviesCard.js
@@ -1,8 +1,11 @@
 import React, { useState } from 'react';
+import { useLocation } from 'react-router-dom';
 import moviePic from '../../../images/movies_pic/4.jpg';
 
 function MoviesCard() {
   const [isLiked, setIsLiked] = useState(false);
+  const location = useLocation();
+  const isSavedMoviesPage = location.pathname === '/saved-movies';
 
   function handleLikeClick() {
     setIsLiked(!isLiked);
@@ -16,13 +19,22 @@ function MoviesCard() {
           дизайна»Киноальманах «100 лет дизайна»
         </p>
         <p className="element__duration">1ч 42м</p>
-        <button
-          className={`element__button button ${
-            isLiked ? 'element__button_active' : ''
-          }`}
-          type="button"
-          onClick={handleLikeClick}
-        />
+
+        {!isSavedMoviesPage && (
+          <button
+            className={`element__button button button_type_like ${
+              isLiked ? 'button_type_like_active' : ''
+            }`}
+            type="button"
+            onClick={handleLikeClick}
+          />
+        )}
+        {isSavedMoviesPage && (
+          <button
+            className="element__button button button_type_remove"
+            type="button"
+          />
+        )}
       </div>
       <div className="element__image-wrapper">
         <img className="element__image" src={moviePic} alt="Обложка фильма" />
