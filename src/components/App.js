ecba294ef3ab64cfe72983647affb1aa41ab355d
Pavel Cherdantsev <ch3pash@yandex.ru>
2023-04-25 23:56:30 +0400

    feat: add routes / /movies /saved-movies and *

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,7 @@
+import { Routes, Route, Navigate } from 'react-router-dom';
+
 import Header from './Header/Header';
-// import Main from './Main/Main';
+import Main from './Main/Main';
 import Movies from './Movies/Movies';
 
 import Footer from './Footer/Footer';
@@ -8,8 +10,14 @@ function App() {
   return (
     <>
       <Header />
-      {/* <Main /> */}
-      <Movies />
+
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/saved-movies" element={<Movies />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+
       <Footer />
     </>
   );
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,12 @@
-// import { NavLink } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 import Navigation from '../Navigation/Navigation';
 
 function Header() {
+  const location = useLocation();
+  const isHomePage = location.pathname === '/';
+
   return (
-    <header className="header header_background_cyan">
+    <header className={`header${isHomePage ? ' header_background_cyan' : ''}`}>
       <div className="header__container page__container">
         <div to="/" alt="Логотип" className="header__logo" />
         <Navigation />
