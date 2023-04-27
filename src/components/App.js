9c7604a12f6f2307f111c0cc84ce2dc927c5c510
Pavel Cherdantsev <ch3pash@yandex.ru>
2023-04-28 00:04:40 +0400

    feat: add Profile component

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import { useEffect, useState } from 'react';
 import Header from './Header/Header';
 import Main from './Main/Main';
 import Movies from './Movies/Movies';
+import Profile from './Profile/Profile';
 
 import Footer from './Footer/Footer';
 import MobileMenu from './MobileMenu/MobileMenu';
@@ -62,6 +63,7 @@ function App() {
         <Route path="/" element={<Main onAnchorBtnClick={scrollToSection} />} />
         <Route path="/movies" element={<Movies />} />
         <Route path="/saved-movies" element={<Movies />} />
+        <Route path="/profile" element={<Profile />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
 
---
+++ src/components/Profile/Profile.js
@@ -0,0 +1,36 @@
+function Profile() {
+  return (
+    <main className="profile">
+      <div className="profile__container">
+        <h1 className="profile__title">Привет, Павел!</h1>
+        <ul className="profile__list page__list">
+          <li className="profile__item">
+            <span className="profile__label">Имя</span>
+            <span className="profile__value">Павел</span>
+          </li>
+          <li className="profile__item">
+            <span className="profile__label">E-mail</span>
+            <span className="profile__value">somemail@yandex.ru</span>
+          </li>
+        </ul>
+      </div>
+
+      <div className="profile__buttons-wrapper">
+        <button
+          className="profile__button button button_type_edit"
+          type="button"
+        >
+          Редактировать
+        </button>
+        <button
+          className="profile__button button button_type_logout"
+          type="button"
+        >
+          Выйти из аккаунта
+        </button>
+      </div>
+    </main>
+  );
+}
+
+export default Profile;
