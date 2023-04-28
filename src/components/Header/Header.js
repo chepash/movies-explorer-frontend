61cdf6e73ac13800149b62baa73a95c7a879c4ee
Pavel Cherdantsev <ch3pash@yandex.ru>
2023-04-28 16:49:23 +0400

    feat: speccial Header for routes /signin and /signup

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,7 +5,7 @@ import Navigation from '../Navigation/Navigation';
 function Header({ loggedIn, onLogoClick, onAccountBtnClick }) {
   const location = useLocation();
   const isHomePage = location.pathname === '/';
-  const isNavigationHidden = ['/signup', '/signin'].includes(location.pathname);
+  const isAuthPage = ['/signup', '/signin'].includes(location.pathname);
 
   // заглушка вместо протектед роутс. тут navigation обязан быть в состоянии loggedIn = true
   const isProtectedRoute = ['/profile', '/movies', '/saved-movies'].includes(
@@ -13,23 +13,40 @@ function Header({ loggedIn, onLogoClick, onAccountBtnClick }) {
   );
 
   return (
-    <header className={`header${isHomePage ? ' header_background_cyan' : ''}`}>
-      <div className="header__container page__container">
-        <img
-          src={logoImage}
-          onClick={onLogoClick}
-          className="header__logo"
-          alt="Логотип"
-          title="Вернуться на главную"
-        />
-        {!isNavigationHidden && (
-          <Navigation
-            onAccountBtnClick={onAccountBtnClick}
-            loggedIn={loggedIn || isProtectedRoute} // заглушка логина
-          />
-        )}
-      </div>
-    </header>
+    <>
+      {!isAuthPage && (
+        <header
+          className={`header${isHomePage ? ' header_background_cyan' : ''}`}
+        >
+          <div className="header__container page__container">
+            <img
+              src={logoImage}
+              onClick={onLogoClick}
+              className="header__logo"
+              alt="Логотип"
+              title="Вернуться на главную"
+            />
+            <Navigation
+              onAccountBtnClick={onAccountBtnClick}
+              loggedIn={loggedIn || isProtectedRoute} // заглушка логина
+            />
+          </div>
+        </header>
+      )}
+      {isAuthPage && (
+        <header className="header">
+          <div className="header__container header__container_auth">
+            <img
+              src={logoImage}
+              onClick={onLogoClick}
+              className="header__logo header__logo_auth"
+              alt="Логотип"
+              title="Вернуться на главную"
+            />
+          </div>
+        </header>
+      )}
+    </>
   );
 }
 
