b3b0ba540d7e1b4fdcbd867a6641257f826d5312
Pavel Cherdantsev <ch3pash@yandex.ru>
2023-04-28 13:10:58 +0400

    feat: temp functionallity for LoggedIn state of navigation

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,6 +6,11 @@ function Header({ loggedIn, onLogoClick, onAccountBtnClick }) {
   const location = useLocation();
   const isHomePage = location.pathname === '/';
 
+  // заглушка вместо протектед роутс. тут navigation обязан быть в состоянии loggedIn = true
+  const isProtectedRout = ['/profile', '/movies', '/saved-movies'].includes(
+    location.pathname
+  );
+
   return (
     <header className={`header${isHomePage ? ' header_background_cyan' : ''}`}>
       <div className="header__container page__container">
@@ -13,11 +18,13 @@ function Header({ loggedIn, onLogoClick, onAccountBtnClick }) {
           src={logoImage}
           onClick={onLogoClick}
           className="header__logo"
-          to="/"
           alt="Логотип"
           title="Вернуться на главную"
         />
-        <Navigation onAccountBtnClick={onAccountBtnClick} loggedIn={loggedIn} />
+        <Navigation
+          onAccountBtnClick={onAccountBtnClick}
+          loggedIn={loggedIn || isProtectedRout} // заглушка логина
+        />
       </div>
     </header>
   );
