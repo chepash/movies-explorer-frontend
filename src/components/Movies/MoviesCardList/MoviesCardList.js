bf826de12c66b3f4a84282134b0678b8f39acafb
Pavel Cherdantsev <ch3pash@yandex.ru>
2023-05-04 23:39:19 +0400

    feat: show only 7 cards with btn 'more'

--- src/components/Movies/MoviesCardList/MoviesCardList.js
+++ src/components/Movies/MoviesCardList/MoviesCardList.js
@@ -1,10 +1,17 @@
+import { useState } from 'react';
 import MoviesCard from '../MoviesCard/MoviesCard';
 
 function MoviesCardList({ filteredCards }) {
+  const [cardsToShow, setCardsToShow] = useState(7);
+
+  const handleShowMoreCards = () => {
+    setCardsToShow(cardsToShow + 7);
+  };
+
   return (
     <section className="elements" aria-label="Список фильмов">
       <ul className="elements__list page__list">
-        {filteredCards.map((card) => (
+        {filteredCards.slice(0, cardsToShow).map((card) => (
           <MoviesCard
             key={card.id}
             card={card}
@@ -18,6 +25,7 @@ function MoviesCardList({ filteredCards }) {
       <button
         className="elements__button button button_type_more"
         type="button"
+        onClick={handleShowMoreCards}
       >
         Ещё
       </button>
