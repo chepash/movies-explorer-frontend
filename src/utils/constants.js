1c5166379f6c8a2e7f813c2869c24eed50b9e5c4
Pavel Cherdantsev <ch3pash@yandex.ru>
2023-05-11 21:27:38 +0400

    feat: change MAIN_API_BASE_URL and homepage in package.json

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,4 @@
 export const MOVIES_API_BASE_URL = 'https://api.nomoreparties.co';
 
-export const MAIN_API_BASE_URL = 'http://localhost:3000';
+export const MAIN_API_BASE_URL =
+  'https://api.chepamovies.nomoredomains.monster';
