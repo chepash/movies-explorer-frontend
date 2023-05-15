693d3177aabe515d90ed88cff842b3cb899c8c8b
Pavel Cherdantsev <ch3pash@yandex.ru>
2023-05-15 23:55:15 +0400

    fix: domain api

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,7 @@
 export const MOVIES_API_BASE_URL = 'https://api.nomoreparties.co';
 
-export const MAIN_API_BASE_URL = 'http://localhost:3000';
+export const MAIN_API_BASE_URL =
+  'https://api.chepamovies.nomoredomains.monster';
 
 export const SHORT_MOVIE_MAX_DURATION = 40;
 
